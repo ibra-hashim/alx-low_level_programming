dc613e786bae710188ff2cfcee7d8fdc31fd461c
ibra-hashim <ibrahoma082@gmail.com>
2023-09-08 11:34:47 -0700

    this is homa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
+ * Description: A C program that print with put function
+ *
  * Return: Always 0 (success)
- */
-int main(void)
+*/
+int  main(void)
 {
 	puts("\"programming is like building amultilingual puzzle");
 	return (0);
